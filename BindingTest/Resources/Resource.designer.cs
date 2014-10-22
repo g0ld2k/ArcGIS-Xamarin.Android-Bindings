22cb8e080a5b2f3ef9cf8da052706682d5ca00c3
Chris Golding <chris.golding@gmail.com>
2014-10-22 14:40:22 -0400

    Misc Fixes

--- BindingTest/Resources/Resource.designer.cs
+++ BindingTest/Resources/Resource.designer.cs
@@ -24,6 +24,10 @@ namespace BindingTest
 			global::Android.Runtime.ResourceIdManager.UpdateIdValues();
 		}
 		
+		public static void UpdateIdValues()
+		{
+		}
+		
 		public partial class Attribute
 		{
 			
